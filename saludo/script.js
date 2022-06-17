26086e46bc524b120d351d32cf9ea2211326915d
Jose Estrella <jcruz_98@outlook.com>
2022-06-17 15:10:44 -0500

    Cambios en mis archivos iniciales

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,16 @@
-console.log("No me faltes el respeto prro");
+console.log("Se nota que aun me extrañas");
+
+setInterval(cambiarSaludo, 9000);
+
+function cambiarSaludo(){
+    //console.log("Goodbye World");
+let frase = document.getElementsByTagName("h3")[0].innerHTML;
+
+
+if(document.getElementsByTagName("h3")[0].innerHTML == "Hello World"){
+    document.getElementsByTagName("h3")[0].innerHTML = "Goodbye World";
+} else {
+    document.getElementsByTagName("h3")[0].innerHTML = "Hello World";
+}
+console.log("frase: " + frase);
+}
